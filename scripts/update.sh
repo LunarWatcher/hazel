b2fa2e4e32c821f37b697391aa77481006382d59
Olivia <zoe.i2k1@gmail.com>
2024-02-15 18:37:44 +0100

    Correct update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,8 @@
 echo "Using username ${HAZEL_USER:=hazel}"
 
-sudo -u $HAZEL_USER git pull && cd build && make -j $(nproc) install
+sudo -u $HAZEL_USER git pull
+cd build 
+sudo -u make -j $(nproc) install
+sudo systemctl restart hazel
 
 
