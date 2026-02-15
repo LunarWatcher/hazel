4193f8623d27128e0c14d498aea30970b7576762
Olivia <oliviawolfie@pm.me>
2026-02-15 05:46:12 +0100

    Make build work again

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,9 @@ cd /opt/hazel
 
 sudo -u $HAZEL_USER git pull
 cd build 
-if ! sudo -u $HAZEL_USER make -j $(nproc) install; then
+# Need to set HOME explicitly in case the user doesn't have a home set. This really should be handled better
+# Could also use /tmp here, but I don't know if that's necessarily desirable
+if ! sudo -u $HAZEL_USER HOME="/home/$HAZEL_USER" make -j $(nproc) install; then
     echo "Failed to install."
     echo "Breaking changes notes:"
     echo "* Installations prior to 2026-02-12 do not use conan, and has more system packages enabled."
